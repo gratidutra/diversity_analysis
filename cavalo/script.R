e489392f41f5e84c233150d269b4d45d3b0415e9
gratidutra <gratirodrigues.gdr@gmail.com>
2022-04-12 11:53:29 -0300

    update script

--- cavalo/script.R
+++ cavalo/script.R
@@ -1,20 +1,25 @@
 # chamando as libs necessárias
 pacman::p_load(tidyverse, magrittr, lubridate, iNEXT, vegan, RAM, permute)
 
-df <- read_csv("data/data.csv")
+df <- read_csv('data.csv')
 df <- df %>% mutate(`Espécie` = case_when(
-  `Espécie` == "Tabanus antarcticus" ~ "Tabanus antarticus",
-  `Espécie` == "Tabaus antarcticus" ~ "Tabanus antarticus",
-  `Espécie` == "Tabanus occidentalis var. dorsovittatus" ~ "Tabanus occidentalis",
-  `Espécie` == "Tabanus occidentalis var. modestus" ~ "Tabanus occidentalis",
-  `Espécie` == "Tabaus occidentalis var. modestus" ~ "Tabanus occidentalis",
-  `Espécie` == "Tabanus occidentalis var.modestus" ~ "Tabanus occidentalis",
-  `Espécie` == "Tabanus occidentalis var. ?dorsovittatus" ~ "Tabanus occidentalis",
-  `Espécie` == "Tabanus rupripes" ~ "Tabanus rubripes",
-  `Espécie` == "Tabanus ?fuscofasciatus" ~ "Tabanus fuscofasciatus",
-  `Espécie` == "Tabanus occiedntalis" ~ "Tabanus occidentalis", TRUE ~ `Espécie`
-))
-
+  `Espécie` == 'Tabanus antarcticus' ~ 'Tabanus antarticus',
+  `Espécie` == 'Tabaus antarcticus' ~ 'Tabanus antarticus',
+  `Espécie` == 'Tabanus occidentalis var. dorsovittatus' ~ 'Tabanus occidentalis',
+  `Espécie` == 'Tabanus occidentalis var. modestus' ~ 'Tabanus occidentalis',
+  `Espécie` == 'Tabaus occidentalis var. modestus' ~ 'Tabanus occidentalis',
+  `Espécie` == 'Tabanus occidentalis var.modestus' ~ 'Tabanus occidentalis',
+  `Espécie` == 'Tabanus occidentalis var. ?dorsovittatus' ~ 'Tabanus occidentalis',
+  `Espécie` == 'Tabanus rupripes' ~ 'Tabanus rubripes',
+  `Espécie` == 'Tabanus ?fuscofasciatus' ~ 'Tabanus fuscofasciatus',
+  `Espécie` == 'Tabanus occiedntalis' ~ 'Tabanus occidentalis', TRUE ~ `Espécie`),
+  `Estação` = case_when(`Estação` == "Chuva" ~ "Rainy",
+                        TRUE ~ "Dry"),
+   Mês = case_when(Mês == 'Agosto' ~ 'August',
+                         Mês == 'Fevereiro' ~ 'February', 
+                         Mês == 'Junho' ~ 'June', 
+                         Mês == 'Maio' ~ 'May', 
+                         Mês == 'Novembro' ~ 'November')) 
 
 # Diagrama de Venn
 
@@ -33,7 +38,7 @@ p5 <- count %>%
 
 
 group.venn(list(Argeu=p1$Espécie, Neucivaldo=p5$Espécie), label=FALSE, 
-           fill = c("lightgray", "darkgray"),
+           fill = c('lightgray', 'darkgray'),
            cat.pos = c(0, 0),
            cex = 2)
 
@@ -46,17 +51,22 @@ top_specie <- df %>% group_by(Espécie) %>%
 
 # Coletas por mês
 
+levels(as.factor(df$Mês))
+
 collect_by_month <- df %>% group_by(Mês, Espécie) %>%
   summarise(total = n()) %>%
   arrange(-total) %>%  filter(total >= 12)
 
+collect_by_month$Mês <- factor(collect_by_month$Mês,
+                 levels = c('November', 'February', 'May', 'June' ,'August'))
+
 ggplot(collect_by_month, aes(Mês, total, fill = total)) +
-  geom_bar(binwidth = 1, stat = "identity") +
+  geom_bar(binwidth = 1, stat = 'identity') +
   theme_light() +
-  scale_fill_gradient(low = "lightgray", high = "black") +
+  scale_fill_gradient(low = 'lightgray', high = 'black') +
   facet_wrap(~Espécie) +
-  ylab("Abundance") +
-  xlab("Month") +
+  ylab('Abundance') +
+  xlab('Month') +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
   coord_polar()
 
@@ -67,29 +77,30 @@ collect_by_hour <- df %>% group_by(hour = hour(`Hora de captura`), Espécie) %>%
   arrange(-total) %>% filter(total > 10)
 
 ggplot(collect_by_hour, aes(hour, total, fill = total)) +
-  geom_bar(binwidth = 1, stat = "identity") +
+  geom_bar(binwidth = 1, stat = 'identity') +
   theme_light() +
-  scale_fill_gradient(low = "lightgray", high = "black") +
+  scale_fill_gradient(low = 'lightgray', high = 'black') +
   scale_x_continuous(breaks = seq(5, 17, by = 2)) +
   facet_wrap(~Espécie) +
-  ylab("Abundance") +
-  xlab("Hour") +
+  ylab('Abundance') +
+  xlab('Hour') +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
   coord_polar()
 
 # Coletas por hora - região do cavalo
 
-collect_by_horse <- df %>% group_by(hour = hour(`Hora de captura`), `Local de pouso`) %>%
+collect_by_horse <- df %>% 
+  group_by(hour = hour(`Hora de captura`), `Local de pouso`) %>%
   summarise(total = n()) %>%
   arrange(-total) %>% filter(total >= 8)
 
 ggplot(collect_by_horse, aes(as.factor(hour), total, fill = total)) +
-  geom_bar(binwidth = 1, stat = "identity") +
+  geom_bar(binwidth = 1, stat = 'identity') +
   theme_light() +
-  scale_fill_gradient(low = "white", high = "black") +
+  scale_fill_gradient(low = 'white', high = 'black') +
   facet_wrap(~`Local de pouso`) +
-  ylab("Abundance") +
-  xlab("Hour") +
+  ylab('Abundance') +
+  xlab('Hour') +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
 
 # Coletas por estação
@@ -98,13 +109,13 @@ collect_by_station <- df %>% group_by(`Estação`, Espécie) %>%
   summarise(total = n()) %>%
   arrange(-total) %>%  filter(total >= 12)
 
-ggplot(collect_by_station, aes(`Estação`, total, fill = total)) +
-  geom_bar(binwidth = 1, stat = "identity") +
+ggplot(collect_by_station, aes(`Estação`, total, fill = `Estação`)) +
+  geom_bar(binwidth = 1, stat = 'identity') +
   theme_light() +
-  scale_fill_gradient(low = "lightgray", high = "black") +
+  #scale_fill_gradient(low = 'lightgray', high = 'black') +
   facet_wrap(~Espécie) +
-  ylab("Abundance") +
-  xlab("Month") +
+  ylab('Abundance') +
+  xlab('Month') +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
 
 # NMDS
@@ -116,9 +127,8 @@ data_nmds <- df %>%
   group_by(Espécie, Localidade, Mês) %>%
   summarise(abund = n()) %>%
   pivot_wider(names_from = Espécie, values_from = abund) %>%
-  mutate(
-    across(everything(), ~ replace_na(.x, 0))
-  ) %>% left_join(rich, by = c("Localidade", "Mês"))
+  replace(is.na(.), 0) %>% 
+  left_join(rich, by = c('Localidade', 'Mês'))
 
 
 run_nmds <- data_nmds
@@ -127,7 +137,7 @@ run_nmds$Localidade <- NULL
 run_nmds$Mês <- NULL
 run_nmds$rich <- NULL
 
-dist_bray <- vegdist(run_nmds, method = "bray", binary = TRUE)
+dist_bray <- vegdist(run_nmds, method = 'bray', binary = TRUE)
 
 dist_bray
 
@@ -139,8 +149,8 @@ scores(nmds)  %>%
   as_tibble()%>%
   ggplot(aes(x = NMDS1, y = NMDS2)) +
   geom_point(aes(size = rich, color = Mês)) +
-  stat_ellipse(geom = "polygon", aes(group = Mês, color = Mês, fill = Mês), alpha = 0.3) +
-  annotate("text", x = -2, y = 0.95, label = paste0("stress: ", format(nmds$stress, digits = 4)), hjust = 0) +
+  stat_ellipse(geom = 'polygon', aes(group = Mês, color = Mês, fill = Mês), alpha = 0.3) +
+  annotate('text', x = -2, y = 0.95, label = paste0('stress: ', format(nmds$stress, digits = 4)), hjust = 0) +
   theme_bw()
 
 # Permanova
@@ -149,14 +159,14 @@ adonis(dist.jac~data_nmds$Localidade, permutations = 1000)
 
 # Rich x Abund
 rich_abund <- data_nmds %>% 
-  pivot_longer(!c(Localidade, Mês, rich), names_to = "Espécie", values_to = "abund") %>% 
-  filter(Espécie %in% c("Tabanus antarcticus","Stypommisa aripuana",
-                        "Pityocera cervus", "Tabanus occidentalis", 
-                        "Tabaus antarcticus"))
+  pivot_longer(!c(Localidade, Mês, rich), names_to = 'Espécie', values_to = 'abund') %>% 
+  filter(Espécie %in% c('Tabanus antarcticus','Stypommisa aripuana',
+                        'Pityocera cervus', 'Tabanus occidentalis', 
+                        'Tabaus antarcticus'))
 
 ggplot(rich_abund, aes(x = rich, y = abund)) +
   geom_point() +
-  geom_smooth(method = "lm", alpha = .15) +
+  geom_smooth(method = 'lm', alpha = .15) +
   facet_wrap(~Espécie)
 
 
@@ -171,11 +181,11 @@ df_climatics <- df %>% group_by(Mês) %>%
 
 ggplot(df_climatics, aes(x = temp, y = total)) +
   geom_point() +
-  geom_smooth(method = "lm", alpha = .15)
+  geom_smooth(method = 'lm', alpha = .15)
 
 ggplot(df_climatics, aes(x = um, y = total)) +
   geom_point() +
-  geom_smooth(method = "lm", alpha = .15)
+  geom_smooth(method = 'lm', alpha = .15)
 
 
 # Rich x Abund
@@ -183,7 +193,7 @@ ggplot(df_climatics, aes(x = um, y = total)) +
 data_rich <- df %>%
   group_by(Localidade, Mês) %>%
   summarise('Temp'= mean(`Temperatura (°C)`),
-            "um" = mean(`Umidade (%)`),
+            'um' = mean(`Umidade (%)`),
             rich = n_distinct(`Espécie`))
 
 data_rich
@@ -193,7 +203,7 @@ data_rich
 
 ggplot(data_rich, aes(x = Temp , y = rich)) +
   geom_point() +
-  geom_smooth(method="glm", family="poisson", se=TRUE)
+  geom_smooth(method='glm', family='poisson', se=TRUE)
 
 
 # Modelo 1
@@ -212,11 +222,11 @@ abund <- df %>%
   summarise(total = n()) %>%
   pivot_wider(names_from = Localidade, values_from = total)  %>%
   replace_na(list(`P1 - Argeu` = 0, `P5-Neucivaldo` = 0)) %>%
-  column_to_rownames(var = "Espécie")
+  column_to_rownames(var = 'Espécie')
 
 resultados_tabanidae <- iNEXT(abund,
                               q = c(0, 1, 2),
-                              datatype = "abundance",
+                              datatype = 'abundance',
                               endpoint = 800
 )
 
@@ -224,38 +234,38 @@ resultados_tabanidae <- iNEXT(abund,
 ## Resultado
 
 ggiNEXT(resultados_tabanidae, 
-        facet.var = "site", 
-        color.var = "order") + 
+        facet.var = 'site', 
+        color.var = 'order') + 
   theme_light()
 
 
 
 # Partes cavalo -----------------------------------------------------------
-df <- read_csv("data/partes_cavalo.csv")
+df <- read_csv('partes_cavalo.csv')
 df <- df %>% mutate(`Espécie` = case_when(
-  `Espécie` == "Tabanus antarcticus" ~ "Tabanus antarticus",
-  `Espécie` == "Tabaus antarcticus" ~ "Tabanus antarticus",
-  `Espécie` == "Tabanus occidentalis var. dorsovittatus" ~ "Tabanus occidentalis",
-  `Espécie` == "Tabanus occidentalis var. modestus" ~ "Tabanus occidentalis",
-  `Espécie` == "Tabaus occidentalis var. modestus" ~ "Tabanus occidentalis",
-  `Espécie` == "Tabanus occidentalis var.modestus" ~ "Tabanus occidentalis",
-  `Espécie` == "Tabanus occidentalis var. ?dorsovittatus" ~ "Tabanus occidentalis",
-  `Espécie` == "Tabanus rupripes" ~ "Tabanus rubripes",
-  `Espécie` == "Tabanus ?fuscofasciatus" ~ "Tabanus fuscofasciatus",
-  `Espécie` == "Tabanus occiedntalis" ~ "Tabanus occidentalis", TRUE ~ `Espécie`
+  `Espécie` == 'Tabanus antarcticus' ~ 'Tabanus antarticus',
+  `Espécie` == 'Tabaus antarcticus' ~ 'Tabanus antarticus',
+  `Espécie` == 'Tabanus occidentalis var. dorsovittatus' ~ 'Tabanus occidentalis',
+  `Espécie` == 'Tabanus occidentalis var. modestus' ~ 'Tabanus occidentalis',
+  `Espécie` == 'Tabaus occidentalis var. modestus' ~ 'Tabanus occidentalis',
+  `Espécie` == 'Tabanus occidentalis var.modestus' ~ 'Tabanus occidentalis',
+  `Espécie` == 'Tabanus occidentalis var. ?dorsovittatus' ~ 'Tabanus occidentalis',
+  `Espécie` == 'Tabanus rupripes' ~ 'Tabanus rubripes',
+  `Espécie` == 'Tabanus ?fuscofasciatus' ~ 'Tabanus fuscofasciatus',
+  `Espécie` == 'Tabanus occiedntalis' ~ 'Tabanus occidentalis', TRUE ~ `Espécie`
 ))
 
-unique(df$`Local de pouso...4`)
+unique(df$`Local de pouso_1`)
 
-df <- df %>% mutate(local_do_pouso_simplificado = case_when(`Local de pouso...4` == "Face" |
-                                                      `Local de pouso...4` == "Pescoço" |
-                                                      `Local de pouso...4` == "Narina" ~ "Cabeça",
-                                                      `Local de pouso...4` == "Barriga" |
-                                                      `Local de pouso...4` == "Aparelho genital"  ~ "Ventral",
-                                                      `Local de pouso...4` == "Rabo" |
-                                                      `Local de pouso...4` == "Quartela traseira"  ~ "Lombar",
-                                                      `Local de pouso...4` == "Perna dianteira" |
-                                                      `Local de pouso...4` == "Perna traseira"  ~ "Pernas",
+df <- df %>% mutate(local_do_pouso_simplificado = case_when(`Local de pouso_1` == 'Face' |
+                                                      `Local de pouso_1` == 'Pescoço' |
+                                                      `Local de pouso_1` == 'Narina' ~ 'Head',
+                                                      `Local de pouso_1` == 'Barriga' |
+                                                      `Local de pouso_1` == 'Aparelho genital'  ~ 'Ventral',
+                                                      `Local de pouso_1` == 'Rabo' |
+                                                      `Local de pouso_1` == 'Quartela traseira'  ~ 'Lumbar',
+                                                      `Local de pouso_1` == 'Perna dianteira' |
+                                                      `Local de pouso_1` == 'Perna traseira'  ~ 'Legs',
                                                       TRUE ~ 'Frontal'
                                                       ))
 unique(df$local_do_pouso_simplificado)
@@ -270,8 +280,9 @@ ggplot(df, aes(as.factor(local_do_pouso_simplificado),
   geom_bar() +
   theme_light() +
   scale_fill_grey(start = 0.25, end = 0.75)+
-  ylab("Abundance") +
-  xlab("Hour") +
+  ylab('Abundance') +
+  xlab('Horse Region') +
   theme(
-    legend.position = "none",
+    legend.position = 'none',
     axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
+
